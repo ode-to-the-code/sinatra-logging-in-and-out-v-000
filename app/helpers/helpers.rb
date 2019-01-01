b8aeb9767f6fea3f3813c956ea9f7d21219540e3
Jad Abdallah <justcallmethis@gmail.com>
2019-01-01 14:48:57 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,6 +4,10 @@ class Helpers
     # binding.pry
     User.find_by_id(session[:user_id])
   end
+  
+  def self.current_user(session)
+    User.find_by_id(session[:session_id])
+  end 
 
   def self.is_logged_in?(session)
     !!session[:user_id]
